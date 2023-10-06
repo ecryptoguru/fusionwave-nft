a3856f1cd6de1d50402f668e9a647bed55a71232
Ankit Das <ecryptoguru@gmail.com>
2023-10-07 01:08:39 +0530

    Update1

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-
-
-export default function Home() {
-  return (
-    <div>
-     <h1 className="text-3xl font-bold underline">
+const Home = () => (
+  <div>
+    <h1 className="text-3xl font-bold underline">
       Hello world!
-     </h1>
-    </div>
-  )
-}
+    </h1>
+  </div>
+);
+
+export default Home;
---
+++ pages/resell-nft.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const ResellNFT = () => (
+  <div>ResellNFT
+  </div>
+);
+
+export default ResellNFT;
