963cd490c340cec5ff85874336fa5d3bd9ac2059
Ankit Das <ecryptoguru@gmail.com>
2023-10-08 03:32:04 +0530

    Update2

--- components/index.js
+++ components/index.js
@@ -2,5 +2,6 @@ import Footer from './Footer';
 import Navbar from './Navbar';
 import Button from './Button';
 import Banner from './Banner';
+import CreatorCard from './CreatorCard';
 
-export { Navbar, Footer, Button, Banner };
+export { Navbar, Footer, Button, Banner, CreatorCard };
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,43 @@
-import { Banner } from '../components';
+import { useEffect, useState, useRef } from 'react';
+import { Banner, CreatorCard } from '../components';
 
-const Home = () => (
+import images from '../assets';
+import { makeid } from '../utils/makeId';
 
-  <div className="flex justify-center sm:px-4 p-12">
-    <div className="w-full minmd:w-4/5">
-      <Banner
-        name={(<>Discover, collect, and sell <br /> extraordinary NFTs</>)}
-        childStyles="md:text-4xl sm:text-2xl xs:text-xl text-left"
-        parentStyle="justify-start mb-7 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
-      />
+const Home = () => {
+  const parentRef = useRef(null);
+  const scrollRef = useRef(null);
+
+  return (
+
+    <div className="flex justify-center sm:px-4 p-12">
+      <div className="w-full minmd:w-4/5">
+        <Banner
+          name={(<>Discover, collect, and sell <br /> extraordinary NFTs</>)}
+          childStyles="md:text-4xl sm:text-2xl xs:text-xl text-left"
+          parentStyle="justify-start mb-7 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
+        />
+        <div>
+          <h1 className="font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold ml-4 xs:ml-0">Best Creators</h1>
+
+          <div className="relative flex-1 max-w-full flex mt-3" ref={parentRef}>
+            <div className="flex flex-row w-max overflow-x-scroll no-scrollbar select-none" ref={scrollRef}>
+
+              {[6, 7, 8, 9, 10].map((i) => (
+                <CreatorCard
+                  key={`creator-${i}`}
+                  rank={i}
+                  creatorImage={images[`creator${i}`]}
+                  creatorName={`0x${makeid(3)}...${makeid(4)}`}
+                  creatorEths={10 - i * 0.534}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Home;
