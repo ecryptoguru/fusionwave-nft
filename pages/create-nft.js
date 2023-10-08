89b3bd2eed2e141a02d07a06207a8cf5f029a707
Ankit Das <ecryptoguru@gmail.com>
2023-10-08 19:26:32 +0530

    minor

--- pages/create-nft.js
+++ pages/create-nft.js
@@ -54,8 +54,15 @@ const CreateItem = () => {
         <h1 className="font-poppins dark:text-white text-nft-black-1 font-semibold text-2xl">
           Create new NFT
         </h1>
-
-        <div className="mt-16">
+        <div className="mt-6">
+          <Button
+            btnName="Generate Image using AI"
+            btnType="primary"
+            classStyles="rounded-xl"
+            handleClick={() => {}}
+          />
+        </div>
+        <div className="mt-12">
           <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-xl">
             Upload file
           </p>
--- pages/index.js
+++ pages/index.js
@@ -49,7 +49,7 @@ const Home = () => {
     <div className="flex justify-center sm:px-4 p-12">
       <div className="w-full minmd:w-4/5">
         <Banner
-          name={(<>Discover, collect, and sell <br /> extraordinary NFTs</>)}
+          name={(<>GenerativeAI enabled NFT Marketplace <br />Create, Discover, collect, and sell </>)}
           childStyles="md:text-4xl sm:text-2xl xs:text-xl text-left"
           parentStyle="justify-start mb-7 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
         />
