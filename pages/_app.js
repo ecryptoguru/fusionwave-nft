a169cf981741c1fc67cf0f17e7a42fada629384f
Ankit Das <ecryptoguru@gmail.com>
2023-10-09 22:03:42 +0530

    Update3

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,23 @@
-import { ThemeProvider } from 'next-themes';
 import Script from 'next/script';
-import { Navbar, Footer } from '../components';
+import { ThemeProvider } from 'next-themes';
+import { NFTProvider } from '../context/NFTContext';
+import { Footer, Navbar } from '../components';
 import '../styles/globals.css';
 
-const MyApp = ({ Component, pageProps }) => (
-  <ThemeProvider attribute="class">
-    <div className="dark:bg-nft-dark bg-white min-h-screen">
-      <Navbar />
-      <div className="pt-65">
-        <Component {...pageProps} />
+const Marketplace = ({ Component, pageProps }) => (
+  <NFTProvider>
+    <ThemeProvider attribute="class">
+      <div className="dark:bg-nft-dark bg-white min-h-screen">
+        <Navbar />
+        <div className="pt-65">
+          <Component {...pageProps} />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
 
-    <Script src="https://kit.fontawesome.com/5c3fbaf330.js" crossOrigin="anonymous" />
-
-  </ThemeProvider>
+      <Script src="https://kit.fontawesome.com/d45b25ceeb.js" crossorigin="anonymous" />
+    </ThemeProvider>
+  </NFTProvider>
 );
 
-export default MyApp;
+export default Marketplace;
