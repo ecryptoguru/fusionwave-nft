e4748c4df598c5f54876a3b6d16f2a4fefcaf1a9
Ankit Das <ecryptoguru@gmail.com>
2023-10-09 17:17:56 +0530

    Update3

--- contract/NFTMarketplace.sol
+++ contract/NFTMarketplace.sol
@@ -45,4 +45,59 @@ contract NFTMarketplace is ERC721URIStorage {
     constructor() ERC721("FusionwaveAI Tokens", "FNFT") {
         owner = payable(msg.sender);
     }
+
+    /* Updates the listing price of the contract */
+    function updateListingPrice(uint _listingPrice) public payable {
+        require(
+            owner == msg.sender,
+            "Only marketplace owner can update listing price."
+        );
+        listingPrice = _listingPrice;
+    }
+
+    //can be updated on frontend
+    function getListingPrice() public view returns (uint256) {
+        return listingPrice;
+    }
+
+    /* Mints a token and lists it in the marketplace */
+    function createToken(
+        string memory tokenURI,
+        uint256 price
+    ) public payable returns (uint) {
+        _tokenIds.increment();
+        uint256 newTokenId = _tokenIds.current();
+        _mint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+        createMarketItem(newTokenId, price);
+        // we've just minted the token and made it sellable
+        return newTokenId;
+    }
+
+    function createMarketItem(uint256 tokenId, uint256 price) private {
+        require(price > 0, "Price must be at least 1 wei");
+        require(
+            msg.value == listingPrice,
+            "Price must be equal to listing price"
+        );
+
+        // create the mapping for the market items
+        idToMarketItem[tokenId] = MarketItem(
+            tokenId,
+            payable(msg.sender),
+            payable(address(this)),
+            price,
+            false
+        );
+
+        // we now want to transfer the ownership of the nft to the contract -> next buyer
+        _transfer(msg.sender, address(this), tokenId);
+        emit MarketItemCreated(
+            tokenId,
+            msg.sender,
+            address(this),
+            price,
+            false
+        );
+    }
 }
