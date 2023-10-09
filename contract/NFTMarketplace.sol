1d92e591f8544e675f8e99e66ac0710584cb2203
Ankit Das <ecryptoguru@gmail.com>
2023-10-09 17:04:10 +0530

    Update3

--- contract/NFTMarketplace.sol
+++ contract/NFTMarketplace.sol
@@ -6,3 +6,43 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 import "hardhat/console.sol";
+
+// Creating our contract ->Inherited from ERC721URIStorage
+contract NFTMarketplace is ERC721URIStorage {
+    // allows us to use the coutner utility.
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIds;
+    Counters.Counter private _itemsSold;
+
+    // fee to list an nft on the marketplace
+    uint256 listingPrice = 0 ether;
+
+    // declaring the owner of the contract
+    address payable owner;
+
+    // pass in the integer which is the item id and it returns a market item.
+    mapping(uint256 => MarketItem) private idToMarketItem;
+
+    struct MarketItem {
+        uint256 tokenId;
+        address payable seller;
+        address payable owner;
+        uint256 price;
+        bool sold;
+    }
+
+    // have an event for when a market item is created.
+    event MarketItemCreated(
+        uint256 indexed tokenId,
+        address seller,
+        address owner,
+        uint256 price,
+        bool sold
+    );
+
+    // set the owner as the msg.sender
+    constructor() ERC721("FusionwaveAI Tokens", "FNFT") {
+        owner = payable(msg.sender);
+    }
+}
