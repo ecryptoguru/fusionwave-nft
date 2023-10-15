8a37672bddbc24758264aea8df0f87a949204c9c
Ankit Das <ecryptoguru@gmail.com>
2023-10-15 14:31:31 +0530

    Update10

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,11 +1,8 @@
 const Banner = ({ name, childStyles, parentStyle, handleClick }) => (
 
   <div onClick={handleClick}>
-    <div className={`relative w-full flex items-center z-0 overflow-hidden bg-gradient-to-br from-purple-600 to-blue-500 
-         hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 ${parentStyle}`}
-    >
-      <p className={`font-bold text-5xl text-white font-poppins leading-70 ${childStyles}`}>{name}
-      </p>
+    <div className={`relative w-full flex items-center z-0 overflow-hidden bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 ${parentStyle}`}>
+      <p className={`font-bold text-5xl text-white font-poppins leading-70 ${childStyles}`}>{name}</p>
       <div className="absolute w-48 h-48 sm:w-32 sm:h-32 rounded-full -top-9 -left-16 -z-5 white-bg" />
       <div className="absolute w-72 h-72 sm:w-56 sm:h-56 rounded-full -bottom-24 -right-14 -z-5 white-bg" />
     </div>
--- pages/index.js
+++ pages/index.js
@@ -103,7 +103,7 @@ const Home = () => {
     <div className="flex justify-center sm:px-4 p-12">
       <div className="w-full minmd:w-4/5">
         <Banner
-          name={(<>GenerativeAI enabled NFT Marketplace <br />Create AI Magic </>)}
+          name={(<>GenerativeAI enabled NFT Marketplace <br />Click to Create AI Magic </>)}
           childStyles="md:text-4xl sm:text-2xl xs:text-xl text-left"
           parentStyle="justify-start mb-7 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
           handleClick={() => window.open('https://fusionimage.netlify.app/')}
