05e8ed00e147646d48d8d5f18a87f60cd1c9facf
Ankit Das <ecryptoguru@gmail.com>
2023-10-19 21:59:19 +0530

    update

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -53,7 +53,8 @@ const ButtonGroup = ({ setActive, router, setIsOpen }) => {
       <Button
         btnName="Create"
         btnType="primary"
-        classStyles="mx-2 rounded-xl"
+        classStyles="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br
+           focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg"
         handleClick={() => {
           setActive('');
           setIsOpen(false);
