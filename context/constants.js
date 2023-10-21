2b8c320c92c430596208efa3e595e4a53c9d0d55
Ankit Das <ecryptoguru@gmail.com>
2023-10-22 02:57:21 +0530

    update

--- context/constants.js
+++ context/constants.js
@@ -3,4 +3,5 @@ import marketAbi from './NFTMarketplace.json';
 export const MarketAddress = '0x6068c6B40DE65BE28CF620642b431287B0CD5450';
 // scrollSepolia MarketAddress = '0xAdFC88d2836f011729324a98c20AD945e0F760c9'
 // mantleTest MarketAddress = '0x0f2C0A451488e87E849B11459146775046Fcf3a4'
+// sepolia MarketAddress = '0x6068c6B40DE65BE28CF620642b431287B0CD5450';
 export const MarketAddressABI = marketAbi.abi;
