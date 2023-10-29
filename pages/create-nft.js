40d4d2bf673630db126fb20027ddf648ba0c8b44
Ankit Das <ecryptoguru@gmail.com>
2023-10-29 19:34:27 +0530

    update

--- pages/create-nft.js
+++ pages/create-nft.js
@@ -96,7 +96,7 @@ const CreateItem = () => {
           btnType="primary"
           classStyles="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br
            focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-          handleClick={() => window.open('https://fusionimage.netlify.app/')}
+          handleClick={() => window.open('https://image.fusionwaveai.com/')}
         />
         <div className="mt-10">
           <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-xl">
